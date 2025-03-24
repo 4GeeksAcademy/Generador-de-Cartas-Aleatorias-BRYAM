dbe23dbfc4ea3d170b5fb3db13d79979c3fa5d9c
bryam <layhamshavii@gmail.com>
2025-03-24 11:16:49 +0000

    carta aleatoria bryam

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+function getRandomCard() {
+  const suits = ['♥', '♠', '♣', '♦'];
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const value = values[Math.floor(Math.random() * values.length)];
+  
+  document.getElementById('top').textContent = suit;
+  document.getElementById('value').textContent = value;
+  document.getElementById('bottom').textContent = suit;
+  
+  document.getElementById('card').style.color = (suit === '♥' || suit === '♦') ? 'black' : 'darkgoldenrod';
+} 
+
+
+window.onload =()=> {
+  getRandomCard();
+  const newCardButton = document.getElementById("botonDeCarta")
+  newCardButton.addEventListener("click",getRandomCard)
+} 
+
